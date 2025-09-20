bc76d56bbabb1a0f2ec7630cd67850125f193b44
T-Sabatier <t.sabatier04@laposte.net>
2025-09-20 15:03:26 +0200

    Création page error + style

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,8 +1,16 @@
+import {NavLink} from 'react-router-dom'
+import "./Error.style.scss"
+
 function Error (){
   return (
-    <p>
-      Erreur
-    </p>
+    <div className= "Erreur">
+    <h2 className ="Erreur__title">404</h2>
+    <h3 className  ="Erreur__Subtitle">Oups ! La page que vous demandez n'existe pas</h3>
+    <NavLink to = "/">
+    Retourner sur la page d'accueil
+    </NavLink>
+    </div>
+    
   )
 }
 export default Error
